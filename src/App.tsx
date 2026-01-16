93e694ada1822c382aea500cc633e06a9c94193a
Vince Balk <blog@vincebalk.nl>
2026-01-16 16:00:00 +0100

    Refactor routing and remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-// Removed conflicting globals.css import - using index.css instead
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+
 import Standings from "./pages/Standings";
 import Statistics from "./pages/Statistics";
-import Login from "./pages/Login";
-import ResetPassword from "./pages/ResetPassword";
 import Dashboard from "./pages/Dashboard";
 import Players from "./pages/Players";
 import PlayerDetail from "./pages/PlayerDetail";
 import Tournaments from "./pages/Tournaments";
-import TournamentStandings from "./pages/TournamentStandings";
 import AssignPlayers from "./pages/AssignPlayers";
 import Matches from "./pages/Matches";
 import Schedule from "./pages/Schedule";
@@ -25,7 +22,6 @@ import Users from "./pages/Users";
 import Profile from "./pages/Profile";
 import Courts from "./pages/Courts";
 import Layout from "./components/Layout";
-import { Footer } from './components/Footer';
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -39,55 +35,82 @@ const App = () => {
         <BrowserRouter>
           <AuthProvider>
             <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/reset-password" element={<ResetPassword />} />
-              <Route path="/" element={
-                <ProtectedRoute>
-                  <Layout />
-                </ProtectedRoute>
-              }>
+              {/* AUTH ROUTES: HARD DISABLED IN TEST REPO */}
+              <Route path="/login" element={<Navigate to="/" replace />} />
+              <Route path="/reset-password" element={<Navigate to="/" replace />} />
+
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Layout />
+                  </ProtectedRoute>
+                }
+              >
                 <Route index element={<Dashboard />} />
                 <Route path="dashboard" element={<Dashboard />} />
                 <Route path="players" element={<Players />} />
                 <Route path="players/:id" element={<PlayerDetail />} />
                 <Route path="tournaments" element={<Tournaments />} />
-                
-                <Route path="tournaments/:id/assign-players" element={
-                  <ProtectedRoute requiredRole="organisator">
-                    <AssignPlayers />
-                  </ProtectedRoute>
-                } />
+
+                <Route
+                  path="tournaments/:id/assign-players"
+                  element={
+                    <ProtectedRoute requiredRole="organisator">
+                      <AssignPlayers />
+                    </ProtectedRoute>
+                  }
+                />
+
                 <Route path="matches" element={<Matches />} />
                 <Route path="schedule" element={<Schedule />} />
                 <Route path="schedule/:tournamentId" element={<Schedule />} />
                 <Route path="standings" element={<Standings />} />
                 <Route path="tournaments/:tournamentId/standings" element={<Standings />} />
                 <Route path="statistics" element={<Statistics />} />
-                
+
                 <Route path="scores" element={<Scores />} />
                 <Route path="scores/:matchId" element={<Scores />} />
-                <Route path="specials" element={
-                  <ProtectedRoute requiredRole="organisator">
-                    <Specials />
-                  </ProtectedRoute>
-                } />
-                <Route path="courts" element={
-                  <ProtectedRoute requiredRole="organisator">
-                    <Courts />
-                  </ProtectedRoute>
-                } />
-                <Route path="settings" element={
-                  <ProtectedRoute requiredRole="organisator">
-                    <Settings />
-                  </ProtectedRoute>
-                } />
+
+                <Route
+                  path="specials"
+                  element={
+                    <ProtectedRoute requiredRole="organisator">
+                      <Specials />
+                    </ProtectedRoute>
+                  }
+                />
+
+                <Route
+                  path="courts"
+                  element={
+                    <ProtectedRoute requiredRole="organisator">
+                      <Courts />
+                    </ProtectedRoute>
+                  }
+                />
+
+                <Route
+                  path="settings"
+                  element={
+                    <ProtectedRoute requiredRole="organisator">
+                      <Settings />
+                    </ProtectedRoute>
+                  }
+                />
+
                 <Route path="profile" element={<Profile />} />
-                <Route path="users" element={
-                  <ProtectedRoute requiredRole="beheerder">
-                    <Users />
-                  </ProtectedRoute>
-                } />
+
+                <Route
+                  path="users"
+                  element={
+                    <ProtectedRoute requiredRole="beheerder">
+                      <Users />
+                    </ProtectedRoute>
+                  }
+                />
               </Route>
+
               <Route path="*" element={<NotFound />} />
             </Routes>
           </AuthProvider>
